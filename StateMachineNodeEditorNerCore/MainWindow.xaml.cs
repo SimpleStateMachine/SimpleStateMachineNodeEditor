3e5309114a1abb462fe85494efd3f059e21a0064
GMIKE <roman.soloweow333@ya.ru>
2020-02-09 02:05:58 +0300

    Add project files.

---
+++ StateMachineNodeEditorNerCore/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StateMachineNodeEditorNerCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            Header.PreviewMouseLeftButtonDown += HeaderClick;
+            ButtonClose.Click += ButtonCloseClick;
+            ButtonMin.Click += ButtonMinClick;
+            ButtonMax.Click += ButtonMaxClick;
+        }
+        void StateNormalMaximaze()
+        {
+            this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+        void ButtonCloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        void ButtonMinClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        void ButtonMaxClick(object sender, RoutedEventArgs e)
+        {
+            StateNormalMaximaze();
+        }
+
+        private void HeaderClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is DockPanel)
+            {
+                if (e.ClickCount == 1)
+                {
+
+                    if (this.WindowState == WindowState.Maximized)
+                    {
+                        var point = PointToScreen(e.MouseDevice.GetPosition(this));
+
+                        if (point.X <= RestoreBounds.Width / 2)
+                            Left = 0;
+                        else if (point.X >= RestoreBounds.Width)
+                            Left = point.X - (RestoreBounds.Width - (this.ActualWidth - point.X));
+                        else
+                            Left = point.X - (RestoreBounds.Width / 2);
+
+                        Top = point.Y - (((FrameworkElement)sender).ActualHeight / 2);
+                        WindowState = WindowState.Normal;
+                    }
+
+                    this.DragMove();
+                }
+                else
+                {
+                    StateNormalMaximaze();
+                }
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ StateMachineNodeEditorNerCore/Styles/CustomWindowTemplate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace StateMachineNodeEditorNerCore.Styles
+{
+    public partial class CustomWindowTemplate
+    {
+        #region sizing event handlers
+
+        void OnSizeSouth(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.South); }
+
+        void OnSizeNorth(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.North); }
+
+        void OnSizeEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.East); }
+
+        void OnSizeWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.West); }
+
+        void OnSizeNorthWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.NorthWest); }
+
+        void OnSizeNorthEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.NorthEast); }
+
+        void OnSizeSouthEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.SouthEast); }
+
+        void OnSizeSouthWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.SouthWest); }
+
+        void OnSize(object sender, SizingAction action)
+        {
+            if (((FrameworkElement)sender).TemplatedParent is Window wnd)
+            {
+                WindowInteropHelper helper = new WindowInteropHelper(wnd);
+                DragSize(helper.Handle, action);
+            }
+        }
+
+        #endregion
+
+        #region P/Invoke and helper method
+
+        const int WM_SYSCOMMAND = 0x112;
+        const int SC_SIZE = 0xF000;
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        void DragSize(IntPtr handle, SizingAction sizingAction)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                SendMessage(handle, WM_SYSCOMMAND, (IntPtr)(SC_SIZE + sizingAction), IntPtr.Zero);
+                SendMessage(handle, 514, IntPtr.Zero, IntPtr.Zero);
+            }
+        }
+
+        #endregion
+
+        #region helper enum
+
+        public enum SizingAction
+        {
+            North = 3,
+            South = 6,
+            East = 2,
+            West = 1,
+            NorthEast = 5,
+            NorthWest = 4,
+            SouthEast = 8,
+            SouthWest = 7
+        }
+
+        #endregion
+    }
+}
